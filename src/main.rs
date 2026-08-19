128f17f00e14eb5c0d65e4895b9c30a5c22a72d2
BowByBoo <rlima.personal@gmail.com>
2026-08-19 15:14:31 -0300

    feat: add standard Forge startup flags

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
+use std::env;
+
 fn main() {
+    match env::args().nth(1).as_deref() {
+        Some("--version") | Some("-V") | Some("version") => {
+            println!("Forge {}", forge::VERSION);
+            return;
+        }
+        Some("--help") | Some("-h") | Some("help") => {
+            forge::print_help();
+            return;
+        }
+        _ => {}
+    }
+
     let mut shell = forge::Shell::new();
 
     if let Err(error) = shell.run() {
